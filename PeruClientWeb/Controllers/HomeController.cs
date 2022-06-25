952c9ed119b10c88a09b0fbf05a3276263ef24e8
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-06-25 15:36:06 -0500

    Agregar archivos de proyecto.

---
+++ PeruClientWeb/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeruClientWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private PeruWs.WebServicePeru ws;
+
+        public HomeController()
+        {
+            ws = new PeruWs.WebServicePeru();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult Departamentos()
+        {
+            var depa = ws.departamentos(); // anytype
+
+            return Json(depa, JsonRequestBehavior.AllowGet); //json
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
